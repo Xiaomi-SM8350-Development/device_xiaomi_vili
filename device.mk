71bfc1e269beed8f1a3870c68b65a79d029d3b8c
Demon Singur <demonsingur@gmail.com>
2019-04-17 16:29:09 +0800

    vili: Import fingerprint HIDL
    Change-Id: I733eea70dc637fa61c2e3a8036100de7febd71a9
    Signed-off-by: TH779 <i@779.moe>
    Signed-off-by: chemusqui1 <61394758+chemusqui1@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -17,6 +17,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_platform_info.xml
 
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_vili
+
 # RRO Overlays
 PRODUCT_PACKAGES += \
     ViliWifiOverlay
