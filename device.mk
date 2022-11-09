62248d9e6eabd2ef1fbfbd33088063513d1cbd60
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2022-11-09 20:15:39 +0100

    vili: Build overlays
    Signed-off-by: chemusqui1 <61394758+chemusqui1@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -21,6 +21,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     ViliWifiOverlay
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
