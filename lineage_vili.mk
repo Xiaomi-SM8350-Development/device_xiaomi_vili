ea31b575bcacf65ca454052dafc37f9bb50fca2c
Arian <arian.kulmer@web.de>
2022-12-31 16:09:26 +0100

    vili: Set stock TARGET_DEVICE and TARGET_PRODUCT
    Change-Id: Ib40dd59a0e8e4df6ade54a6a7a7434bfb7a06409
    Signed-off-by: chemusqui1 <61394758+chemusqui1@users.noreply.github.com>

--- lineage_vili.mk
+++ lineage_vili.mk
@@ -22,8 +22,13 @@ PRODUCT_NAME := lineage_vili
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+PRODUCT_SYSTEM_NAME := vili_global
+PRODUCT_SYSTEM_DEVICE := vili
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vili_global-user 12 SKQ1.211006.001 V13.0.8.0.SKDMIXM release-keys"
+    PRIVATE_BUILD_DESC="vili_global-user 12 SKQ1.211006.001 V13.0.8.0.SKDMIXM release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := Xiaomi/vili_global/vili:12/SKQ1.211006.001/V13.0.8.0.SKDMIXM:user/release-keys
