7f9bac8798f674bc58904ab98d6a2679d61b9650
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2023-01-25 00:33:23 +0100

    vili: Inherit from common tree
    Signed-off-by: chemusqui1 <61394758+chemusqui1@users.noreply.github.com>

---
+++ device.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sm8350-common
+$(call inherit-product, device/xiaomi/sm8350-common/common.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
