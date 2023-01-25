1819904d14c7152198efa55ba948989a0b30f323
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2023-01-25 01:06:22 +0100

    vili: Add OTA assert
    Signed-off-by: chemusqui1 <61394758+chemusqui1@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/xiaomi/vili
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := vili
+
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := vili
 
