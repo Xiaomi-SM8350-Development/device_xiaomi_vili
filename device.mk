770768820956b6fc00dbe64a3eba10202b647ed3
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2023-01-25 20:50:28 +0100

    vili: Copy FP permissions

--- device.mk
+++ device.mk
@@ -22,6 +22,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_vili
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # RRO Overlays
 PRODUCT_PACKAGES += \
     ViliWifiOverlay
