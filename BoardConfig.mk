d846c13e2053a85ec70c4288a5d28b21a8c18d65
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2023-01-30 22:50:02 +0100

    vili: Include device-specific kernel fragment
    Signed-off-by: chemusqui1 <61394758+chemusqui1@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,5 +9,8 @@ DEVICE_PATH := device/xiaomi/vili
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
+# Kernel
+TARGET_KERNEL_CONFIG += vendor/vili_QGKI.config
+
 # Inherit from proprietary files
 include vendor/xiaomi/vili/BoardConfigVendor.mk
